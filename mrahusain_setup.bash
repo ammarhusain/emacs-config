bade62a14ac6b7ecab80b69e244b3bb53597f774
Ammar Husain <ammar_husain@apple.com>
2016-02-08 17:36:04 -0800

    Added the git branch display to the bash script

--- mrahusain_setup.bash
+++ mrahusain_setup.bash
@@ -1,4 +1,17 @@
+# Print current git branch on the terminal
+parse_git_branch() {
+    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
+
+export PS1="\u@\h \W\[\033[32m\]\$(parse_git_branch)\[\033[00m\] $ "
+if [ -f ~/.git-completion.bash ]; then
+    . ~/.git-completion.bash
+fi
+
+# set git editor to emacs
 export EDITOR=emacs
+
+# set your aliases
 alias e="emacs"
 alias e2="emacs -nw"
 alias end="emacs --no-desktop"
