be36ef1d77d2f689440ddc315593940a8292e5d2
ammar (testos) <ammar@testos.virtualbox>
2018-09-25 19:21:08 -0700

    Cleaned out a bunch of unnecessary functionality Removed Rtags & Cmake-ide: too much overhead Relying entirely on irony mode + company Will use grep & ag to search for files & definitions

--- cpp_intellisense_sandbox/main.cpp
+++ cpp_intellisense_sandbox/main.cpp
@@ -1,9 +1,15 @@
-#include <my_base.h>
+#include "my_base.h"
 #include "derived_1.h"
 
 #include <stdio.h>
 #include <string>
 #include <iostream>
+#include <vector>
+
+struct TH1F {
+  TH1F(int a) {}
+  void foo() {}
+};
 
 int main() {
   std::string a("ammar");
@@ -12,6 +18,9 @@ int main() {
   mb.MustDefine();
   mb.MustDefine_arg(2);
   Derived_1 dr;
-  MyBase* d_1 = new Derived_1;
+  MyBase *d_1 = new Derived_1;
   d_1->MustDefine();
+  std::string str;
+  TH1F *h1 = new TH1F(5);
+  h1->foo();
 }
--- elpa/cl-lib-0.6.1/cl-lib-autoloads.el
+++
@@ -1,23 +0,0 @@
-;;; cl-lib-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "cl-lib" "cl-lib.el" (22828 18380 0 0))
-;;; Generated autoloads from cl-lib.el
- (let ((d (file-name-directory #$)))
-  (when (member d load-path)
-    (setq load-path (append (remove d load-path) (list d)))))
-
-;;;***
-
-;;;### (autoloads nil nil ("cl-lib-pkg.el") (22828 18362 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; cl-lib-autoloads.el ends here
