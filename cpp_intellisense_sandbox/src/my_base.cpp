b97a3287772a4e6e8b2122e8b9b0d3844c6cb0f2
Ammar Husain <mrahusain@gmail.com>
2018-10-05 18:25:59 -0700

    Updated C++ example to play around in GDB

--- cpp_intellisense_sandbox/src/my_base.cpp
+++ cpp_intellisense_sandbox/src/my_base.cpp
@@ -5,15 +5,15 @@
 using std::string;
 
 MyBase::MyBase() {
+  letters_.resize(4);
+  letters_ = {'a', 'b', 'c', 'd'};
   std::cout << "Constructor" << std::endl;
   MustDefine();
 }
 
-void MyBase::MustDefine() {
-  std::cout << "Implementation of MustDefine()" << std::endl;
-}
+void MyBase::MustDefine() { std::cout << "Implementation of MustDefine()" << std::endl; }
 
 double MyBase::MustDefine_arg(int a) {
-  string st;
+  string st("ammar");
   return a;
 }
